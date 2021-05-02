b2ebce6136573bf1d9e12027851906ccdc3145ad
Brian Haddad <brianhaddad@gmail.com>
2021-05-01 22:55:46 -0700

    Building out the options my daughter wanted in here.

--- SimpleFortuneTeller/SimpleFortuneTeller/Program.cs
+++ SimpleFortuneTeller/SimpleFortuneTeller/Program.cs
@@ -13,54 +13,180 @@ namespace SimpleFortuneTeller
             {
                 new FortuneOption
                 {
-                    OptionDisplay = "Option 1",
-                    ForegroundColor = ConsoleColor.White,
-                    BackgroundColor = ConsoleColor.DarkBlue,
+                    OptionDisplay = "Harry Potter",
+                    ForegroundColor = ConsoleColor.Yellow,
+                    BackgroundColor = ConsoleColor.DarkRed,
                     NextOptions = new List<FortuneOption>
                     {
                         new FortuneOption
                         {
-                            OptionDisplay = "Option A",
-                            ForegroundColor = ConsoleColor.Green,
-                            BackgroundColor = ConsoleColor.Black,
+                            OptionDisplay = "Cedric",
+                            ForegroundColor = ConsoleColor.DarkBlue,
+                            BackgroundColor = ConsoleColor.Yellow,
                         },
                         new FortuneOption
                         {
-                            OptionDisplay = "Option B",
+                            OptionDisplay = "Draco",
+                            ForegroundColor = ConsoleColor.White,
+                            BackgroundColor = ConsoleColor.DarkGreen,
+                        },
+                        new FortuneOption
+                        {
+                            OptionDisplay = "Cho Chang",
+                            ForegroundColor = ConsoleColor.Cyan,
+                            BackgroundColor = ConsoleColor.DarkBlue,
+                        },
+                        new FortuneOption
+                        {
+                            OptionDisplay = "Luna Lovegood",
                             ForegroundColor = ConsoleColor.DarkYellow,
-                            BackgroundColor = ConsoleColor.Cyan,
+                            BackgroundColor = ConsoleColor.Blue,
+                        },
+                        new FortuneOption
+                        {
+                            OptionDisplay = "Myrtle",
+                            ForegroundColor = ConsoleColor.White,
+                            BackgroundColor = ConsoleColor.DarkCyan,
+                        },
+                        new FortuneOption
+                        {
+                            OptionDisplay = "Madam Pomfrey",
+                            ForegroundColor = ConsoleColor.Black,
+                            BackgroundColor = ConsoleColor.White,
                         },
                     },
                 },
                 new FortuneOption
                 {
-                    OptionDisplay = "Option 2",
-                    ForegroundColor = ConsoleColor.Black,
-                    BackgroundColor = ConsoleColor.Cyan,
-                },
-                new FortuneOption
-                {
-                    OptionDisplay = "Really Long Option 3",
-                    ForegroundColor = ConsoleColor.Cyan,
-                    BackgroundColor = ConsoleColor.DarkYellow,
-                },
-                new FortuneOption
-                {
-                    OptionDisplay = "Option 4",
+                    OptionDisplay = "Dexter's Laboratory",
                     ForegroundColor = ConsoleColor.Green,
                     BackgroundColor = ConsoleColor.DarkBlue,
+                    NextOptions = new List<FortuneOption>
+                    {
+                        new FortuneOption
+                        {
+                            OptionDisplay = "Dexter",
+                            ForegroundColor = ConsoleColor.White,
+                            BackgroundColor = ConsoleColor.DarkBlue,
+                        },
+                        new FortuneOption
+                        {
+                            OptionDisplay = "Dee Dee",
+                            ForegroundColor = ConsoleColor.Yellow,
+                            BackgroundColor = ConsoleColor.Magenta,
+                        },
+                        new FortuneOption
+                        {
+                            OptionDisplay = "Mandark",
+                            ForegroundColor = ConsoleColor.Blue,
+                            BackgroundColor = ConsoleColor.DarkYellow,
+                        },
+                    },
                 },
                 new FortuneOption
                 {
-                    OptionDisplay = "Option 5",
+                    OptionDisplay = "My Hero Academia",
+                    ForegroundColor = ConsoleColor.DarkBlue,
+                    BackgroundColor = ConsoleColor.Yellow,
+                    NextOptions = new List<FortuneOption>
+                    {
+                        new FortuneOption
+                        {
+                            OptionDisplay = "Deku",
+                            ForegroundColor = ConsoleColor.DarkBlue,
+                            BackgroundColor = ConsoleColor.Green,
+                        },
+                        new FortuneOption
+                        {
+                            OptionDisplay = "Bakugo",
+                            ForegroundColor = ConsoleColor.Red,
+                            BackgroundColor = ConsoleColor.DarkCyan,
+                        },
+                        new FortuneOption
+                        {
+                            OptionDisplay = "Dabi",
+                            ForegroundColor = ConsoleColor.White,
+                            BackgroundColor = ConsoleColor.DarkBlue,
+                        },
+                        new FortuneOption
+                        {
+                            OptionDisplay = "Kirishima",
+                            ForegroundColor = ConsoleColor.Cyan,
+                            BackgroundColor = ConsoleColor.Red,
+                        },
+                    },
+                },
+                new FortuneOption
+                {
+                    OptionDisplay = "Disney",
                     ForegroundColor = ConsoleColor.White,
-                    BackgroundColor = ConsoleColor.DarkMagenta,
+                    BackgroundColor = ConsoleColor.Blue,
+                    NextOptions = new List<FortuneOption>
+                    {
+                        new FortuneOption
+                        {
+                            OptionDisplay = "Tinkerbell",
+                            ForegroundColor = ConsoleColor.Yellow,
+                            BackgroundColor = ConsoleColor.DarkGreen,
+                        },
+                        new FortuneOption
+                        {
+                            OptionDisplay = "Aladdin",
+                            ForegroundColor = ConsoleColor.Black,
+                            BackgroundColor = ConsoleColor.Blue,
+                        },
+                        new FortuneOption
+                        {
+                            OptionDisplay = "Ariel",
+                            ForegroundColor = ConsoleColor.Green,
+                            BackgroundColor = ConsoleColor.DarkRed,
+                        },
+                        new FortuneOption
+                        {
+                            OptionDisplay = "Prince Charming",
+                            ForegroundColor = ConsoleColor.Yellow,
+                            BackgroundColor = ConsoleColor.DarkBlue,
+                        },
+                        new FortuneOption
+                        {
+                            OptionDisplay = "Rapunzel",
+                            ForegroundColor = ConsoleColor.Yellow,
+                            BackgroundColor = ConsoleColor.DarkMagenta,
+                        },
+                        new FortuneOption
+                        {
+                            OptionDisplay = "Gaston",
+                            ForegroundColor = ConsoleColor.DarkYellow,
+                            BackgroundColor = ConsoleColor.DarkRed,
+                        },
+                    },
                 },
                 new FortuneOption
                 {
-                    OptionDisplay = "Option 6",
-                    ForegroundColor = ConsoleColor.Magenta,
-                    BackgroundColor = ConsoleColor.Black,
+                    OptionDisplay = "Lord of the Rings",
+                    ForegroundColor = ConsoleColor.Yellow,
+                    BackgroundColor = ConsoleColor.DarkGreen,
+                    NextOptions = new List<FortuneOption>
+                    {
+                        new FortuneOption
+                        {
+                            OptionDisplay = "Smeagol",
+                            ForegroundColor = ConsoleColor.Red,
+                            BackgroundColor = ConsoleColor.DarkCyan,
+                        },
+                        new FortuneOption
+                        {
+                            OptionDisplay = "Legolas",
+                            ForegroundColor = ConsoleColor.Yellow,
+                            BackgroundColor = ConsoleColor.DarkGreen,
+                        },
+                        new FortuneOption
+                        {
+                            OptionDisplay = "Tauriel",
+                            ForegroundColor = ConsoleColor.Red,
+                            BackgroundColor = ConsoleColor.DarkBlue,
+                        },
+                    },
                 },
             };
             FortuneLogic.AddFortuneOptions(fortunes);
